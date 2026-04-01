e8355a483b84686542f76eb14af76a9c8465ac8e
DeltaStudent-2002 <maheshdhondge26@gmail.ocm>
2026-04-01 12:51:37 +0530

    MiniProject/notes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,54 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
 
 const app = express();
 dotenv.config();
-const PORT = process.env.PORT 
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+const PORT = process.env.PORT;
+let notes = [];
+app.use(express.json()); //To read json data data in req.body
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
 
-app.get('/home', (req, res)=>{
-    res.send('Welcome to the Home Page!');
+app.post("/notes", (req, res) => {
+  console.log(req.body);
+  notes.push(req.body);
+  res.json({
+    message: "Notes added successfully",
+    notes: notes,
+  });
+});
+
+app.delete("/notes/:index", (req, res) => {
+  const index = req.params.index;
+  delete notes[index];
+  res.json({
+    message: "note deleted successfully",
+    notes: notes,
+  });
+});
+
+app.patch("/notes/:index",(req, res)=>{
+    const index = req.params.index;
+    const {title} = req.body
+    const {description} = req.body
+
+    notes[index].title = title
+    notes[index].description = description
+    res.json({
+        message: "note updated successfully",
+        notes: notes,
+    })
 })
 
-app.get('/about', (req, res)=>{
-    res.send('This is the About Page!');
-})
+
+app.get("/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/about", (req, res) => {
+  res.send("This is the About Page!");
+});
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
